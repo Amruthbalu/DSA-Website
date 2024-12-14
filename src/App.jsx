68ce67acdf6f3631569eee7fa990715ee3aad0ab
Amruth <you@example.com>
2024-12-14 16:30:44 +0530

    14/12/24 First Line of Code

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <ul>DSA Website Under Development</ul>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
